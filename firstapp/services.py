efd86eaa4f72c50717ef77048912a1da7621e735
Hammad <hammmad220@gmail.com>
2026-04-14 09:10:25 +0500

    fix

--- firstapp/services.py
+++ firstapp/services.py
@@ -1,10 +1,10 @@
 from .models import Reservation
 
 def create_reservation(validated_data):
-    name = validated_data.get('first_name')
-    time = validated_data.get('reservation_time')
+    first_name = validated_data.get('first_name')
+    reservation_time = validated_data.get('reservation_time')
 
-    if Reservation.objects.filter(first_name=name, time=time).exists():
+    if Reservation.objects.filter(first_name=first_name, time=reservation_time).exists():
         raise Exception("Slot already booked")
     
     return Reservation.objects.create(**validated_data)
