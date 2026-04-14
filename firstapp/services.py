57d224073d72aa471bcc2d5e3b883a83c5483ba7
Hammad <hammmad220@gmail.com>
2026-04-14 09:22:30 +0500

    time field fix

--- firstapp/services.py
+++ firstapp/services.py
@@ -4,7 +4,7 @@ def create_reservation(validated_data):
     first_name = validated_data.get('first_name')
     reservation_time = validated_data.get('reservation_time')
 
-    if Reservation.objects.filter(first_name=first_name, time=reservation_time).exists():
+    if Reservation.objects.filter(first_name=first_name, reservation_time=reservation_time).exists():
         raise Exception("Slot already booked")
     
     return Reservation.objects.create(**validated_data)
